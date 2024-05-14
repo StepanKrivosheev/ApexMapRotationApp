51cbd374ab7f83843f90126f817b8245884bdc8d
StepanKrivosheev <92262136+StepanKrivosheev@users.noreply.github.com>
2024-05-14 20:40:02 +0200

    Fixes

--- src/bot.js
+++ src/bot.js
@@ -1,75 +1,108 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
-const {
-  Client,
-  Intents,
-  ShardClientUtil,
-  Activity,
-  Presence,
-} = require("discord.js");
-const getMapInfo = require("./getMapInfo");
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+const getMapInfo = require('./getMapInfo');
+const getMapInfoR = require('./getMapInfoRanked');
+
 dotenv.config();
+botToken = process.env.BOT_TOKEN;
 
-mapTime = "0";
+const discordChannels = process.env.DISCORD_CHANNELS;
+const channelsArray = discordChannels.split(',');
+
+let mapTimeC = '0';
+let mapTimeR = '0';
+let rankedInfo = '0';
 
 module.exports = async function bot() {
   const client = new Client({
     intents: [
-      Intents.FLAGS.GUILDS,
-      Intents.FLAGS.GUILD_MESSAGES,
-      Intents.FLAGS.GUILD_PRESENCES,
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildPresences,
     ],
   });
-  client.login(process.env.DSICORDJS_BOT_TOKEN);
-  prefix = "-";
+  client.login(botToken);
 
-  await client.on("ready", async () => {
-    //gets Map info from 'apexlaegendsstatus.com'
-    let mapInfo = await getMapInfo();
-
-    mapTime = mapInfo.info.slice(7, 9) + mapInfo.info.slice(10, 12);
-    map = mapInfo.nickname;
-
-    haltDieText = client.channels.cache.get(haltDieText);
-
+  async function updateNickname(nickname, info, rankedInfo) {
     //sets description on discord to map info and status to 'do not disturb'
-    await client.user.setActivity(mapInfo.info, { type: "PLAYING" });
-    await client.user.setStatus("dnd");
+    // await client.user.setActivity(info, { type: 'PLAYING' });
+    // await client.user.setStatus('dnd');
 
-    //sends command to self and sets own nickname to currenr map
-    await haltDieText.send("@silent " + prefix + "setnick " + mapInfo.nickname);
-    await client.on("messageCreate", async (message) => {
-      if (message.content.startsWith(prefix + "setnick")) {
-        //message.content.slice(("setnick").length);
-        await message.member.setNickname(map);
-        setTimeout(() => message.delete(), 100);
+    client.user.setPresence({
+      status: 'dnd',
+      activities: [
+        {
+          type: ActivityType.Custom,
+          name: info,
+        },
+      ],
+    });
+
+    client.channels.cache.get(channelsArray[0]).send('-setnick ' + nickname);
+    client.on('messageCreate', async (message) => {
+      if (message.content.startsWith('-setnick')) {
+        await message.member.setNickname(nickname);
+        //await client.user.setAvatar()
+        await message.channel.bulkDelete(1);
+        await channelsArray.forEach((element) => {
+          client.channels.cache.get(element).setTopic('RANKED MAP: ' + rankedInfo);
+        });
       }
     });
+  }
+
+  await client.on('ready', async () => {
+    let mapInfo = await getMapInfo();
+
+    mapTimeC = mapInfo.mapTimeC;
+    mapTimeR = mapInfo.mapTimeR;
+    let rankedInfo = mapInfo.nicknameR + mapInfo.infoR;
+
+    updateNickname(mapInfo.nicknameC, mapInfo.infoC, rankedInfo);
+  });
+
+  client.on('messageCreate', async (message) => {
+    if (message.content.match('/update')) {
+      await client.channels.cache.get(message.channelId).send('Updating....');
+      await message.delete(message);
+
+      //gets Map info from 'apexlaegendsstatus.com'
+      let mapInfo = await getMapInfo();
+      mapTimeC = mapInfo.mapTimeC;
+
+      rankedInfo = mapInfo.nicknameR + mapInfo.infoR;
+
+      updateNickname(mapInfoC.nicknameC, mapInfoC.infoC, rankedInfo);
+    }
+
+    if (message.content.startsWith('-delete')) {
+      let x = 0;
+      x = message.toString().slice(8);
+      await message.channel.bulkDelete(x);
+    }
   });
 
   //checks the time every minute
+
   setInterval(async () => {
     time = Date(Date.now()).toString();
-    time = time.slice(16, 18) + time.slice(19, 21);
+    time = time.slice(16, 18) + ':' + time.slice(19, 21);
 
-    //starts code at half/full houre
-    if (time === mapTime) {
+    //starts if maptime = time
+    if (time === '19:00') {
+      let mapInfo = await getMapInfo();
+      mapTimeC = mapInfo.mapTimeC;
+      rankedInfo = mapInfo.nicknameR + mapInfo.infoR;
+      updateNickname(mapInfo.nicknameC, mapInfo.infoC, rankedInfo);
+    }
+    if (time === mapTimeC) {
       //gets Map info from 'apexlaegendsstatus.com'
       let mapInfo = await getMapInfo();
+      mapTimeC = mapInfo.mapTimeC;
+      rankedInfo = mapInfo.nicknameR + mapInfo.infoR;
 
-      //sets description on discord to map info
-      client.user.setActivity(mapInfo.info, { type: "PLAYING" });
-
-      //sends command to self and sets own nickname to currenr map
-      haltDieText.send("@silent " + prefix + "setnick " + mapInfo.nickname);
-
-      client.on("messageCreate", async (message) => {
-        if (message.content.startsWith(prefix + "setnick")) {
-          //message.content.slice(("setnick").length);
-          message.member.setNickname(mapInfo.nickname);
-          setTimeout(() => message.delete(), 100);
-        }
-      });
+      updateNickname(mapInfo.nicknameC, mapInfo.infoC, rankedInfo);
     }
-  }, 60 * 1000);
+  }, 60 /*sec*/ * 1000 /*millisec*/);
 };
--- src/getMapInfo.js
+++ src/getMapInfo.js
@@ -1,28 +1,39 @@
-const dotenv = require("dotenv");
-const puppeteer = require("puppeteer");
-const fs = require("fs/promises");
-const time = require("./time");
+const puppeteer = require('puppeteer');
 
 module.exports = async function start() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(
-    "https://apexlegendsstatus.com/current-map/battle_royale/pubs"
+  page.setDefaultNavigationTimeout(0);
+  await page.goto('https://apexlegendsstatus.com/current-map');
+
+  const maps = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('h2'), (e) => e.innerText)
   );
 
-  const maps = await page.$$eval(".row h3", (maps) => {
-    return maps.map((x) => x.textContent);
-  });
-
-  const times = await page.$$eval(".row p", (times) => {
-    return times
-      .map((x) => x.textContent.slice(11, 19).replace(/to/, "till "))
-      .slice(1, 12);
-  });
+  const times = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('h5'), (e) => e.innerText)
+  );
 
   await browser.close();
-  let info = ` ${times[0]} -> ${maps[1]}`;
-  let nickname = maps[0];
+  let nextMap = times[1].substring(12, times[1].indexOf(','));
+  let infoC = ` till ${times[0].slice(14)} -> ${nextMap}`;
+  let nicknameC = maps[1];
+  let mapTimeC = infoC.slice(6, 10) + infoC.slice(10, 12);
+  await browser.close();
+  let infoR = ` -> ${times[3].substring(12, times[3].indexOf(','))}`;
+  let nicknameR = maps[3];
+  let mapTimeR = infoR.slice(7, 9) + infoR.slice(10, 12);
+  let nextRankedMap = times[3].substring(12, times[3].indexOf(','));
+  let nextMaps = [nicknameC, nicknameR, nextMap, nextRankedMap];
 
-  return { info, nickname };
+  for (e in [nicknameC, nicknameR, nextMap, nextRankedMap]) {
+    if (e === 'Kings Canyon') e = e.concat('👑');
+    else if (e === 'Olympus') e = e.concat('🏛️');
+    else if (e === 'Worlds Edge') e = e.concat('❄️');
+    else if (e === 'Storm Point') e = e.concat('🦀');
+    else if (e === 'Broken Moon') e = e.concat('☄️');
+  }
+
+  nicknameC.concat;
+  return { infoC, nicknameC, mapTimeC, infoR, nicknameR, mapTimeR };
 };

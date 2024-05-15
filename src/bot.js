e0fefff5524420092bf5e6759f637228d0f47679
StepanKrivosheev <92262136+StepanKrivosheev@users.noreply.github.com>
2024-05-16 01:53:40 +0200

    some more fixes

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 
-const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType, MessageCollector } = require('discord.js');
 const getMapInfo = require('./getMapInfo');
 
 dotenv.config();
@@ -10,90 +10,78 @@ const discordChannels = process.env.DISCORD_CHANNELS;
 const channelsArray = discordChannels.split(',');
 
 let mapTimeC = '0';
-let rankedInfo = '0';
+let timeOut = '15';
 
 module.exports = async function bot() {
-	const client = new Client({
-		intents: [
-			GatewayIntentBits.Guilds,
-			GatewayIntentBits.GuildMessages,
-			GatewayIntentBits.GuildPresences,
-		],
-	});
-	client.login(botToken);
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildPresences,
+      GatewayIntentBits.MessageContent,
+    ],
+  });
+  client.login(botToken);
 
-	async function updateNickname() {
-		//sets description on discord to map info and status to 'do not disturb'
-		// await client.user.setActivity(info, { type: 'PLAYING' });
-		// await client.user.setStatus('dnd');
+  async function updateNickname() {
+    let mapInfo = await getMapInfo();
+    mapTimeC = mapInfo.mapTimeC;
 
-		let mapInfo = await getMapInfo();
-		mapTimeC = mapInfo.mapTimeC;
+    client.user.setPresence({
+      status: 'dnd',
+      activities: [
+        {
+          type: ActivityType.Custom,
+          name: mapInfo.infoC,
+        },
+      ],
+    });
+    client.channels.cache
+      .get(channelsArray[0])
+      .members.get(client.user.id)
+      .setNickname(mapInfo.curentMap);
 
-		client.user.setPresence({
-			status: 'dnd',
-			activities: [
-				{
-					type: ActivityType.Custom,
-					name: mapInfo.infoC,
-				},
-			],
-		});
+    if (timeOut <= 0) {
+      await client.user.setAvatar(mapInfo.avatar);
+      await client.user.setBanner(mapInfo.avatar);
+    }
 
-		client.channels.cache
-			.get(channelsArray[0])
-			.send('-setnick ' + mapInfo.curentMap);
-		client.on('messageCreate', async (message) => {
-			if (message.content.startsWith('-setnick')) {
-        
-				await message.member.setNickname(mapInfo.curentMap);
-				//await message.member.roles.color.setColor('DarkGreen');
-				await client.user.setAvatar(mapInfo.avatar);
-				await message.channel.bulkDelete(1);
-				await channelsArray.forEach((element) => {
-					client.channels.cache
-						.get(element)
-						.setTopic('R.M.: ' + mapInfo.infoR);
-				});
-			}
-		});
-	}
+    await channelsArray.forEach((element) => {
+      client.channels.cache.get(element).setTopic('R.M.: ' + mapInfo.infoR);
+    });
+  }
 
-	await client.on('ready', async () => {
-		updateNickname();
-	});
+  await client.on('ready', async () => {
+    updateNickname();
+  });
 
-	client.on('messageCreate', async (message) => {
-		if (message.content.match('/update')) {
+  await client.on('messageCreate', async (message) => {
+    if (message.content.match('/update')) {
+      await client.channels.cache.get(message.channelId).send('Updating....');
+      await updateNickname();
+      await message.channel.bulkDelete(2);
+    }
 
-			await client.channels.cache.get(message.channelId).send('Updating....');
-			await message.delete(message);
-			
-			updateNickname();
-		}
+    if (message.content.startsWith('/delete')) {
+      let x = 0;
+      x = message.toString().slice(8, 10);
+      await message.channel.bulkDelete(x);
+    }
+  });
 
-		if (message.content.startsWith('-delete')) {
-      console.log(message.content);
-			let x = 0;
-			x = message.toString().slice(8);
-			await message.channel.bulkDelete(x);
-		}
-	});
+  //checks the time every minute
 
-	//checks the time every minute
+  setInterval(async () => {
+    timeOut--;
+    time = Date(Date.now()).toString();
+    time = time.slice(16, 18) + ':' + time.slice(19, 21);
 
-	setInterval(async () => {
-		time = Date(Date.now()).toString();
-		time = time.slice(16, 18) + ':' + time.slice(19, 21);
-
-		//starts if maptime = time
-		if (time === '19:00') {
-
-      updateNickname();
-		}
-		if (time === mapTimeC) {
-		
-			updateNickname();
-		}
-	}, 60 /*sec*/ * 1000 /*millisec*/);
+    //starts if maptime = time
+    if (time === '19:00') {
+      await updateNickname();
+    }
+    if (time === mapTimeC) {
+      await updateNickname();
+    }
+  }, 10 /*sec*/ * 1000 /*millisec*/);
 };
--- src/getMapInfo.js
+++ src/getMapInfo.js
@@ -19,32 +19,41 @@ module.exports = async function start() {
   let nextMap = times[1].substring(12, times[1].indexOf(','));
   let curentRankedMap = maps[3];
   let nextRankedMap = times[3].substring(12, times[3].indexOf(','));
-  let avatar ='';
+  let avatar = '';
 
-  if      (curentMap === 'Kings Canyon') avatar = ('https://static.wikia.nocookie.net/apexlegends_gamepedia_en/images/2/2d/Transition_Kings_Canyon_MU4.png/revision/latest/scale-to-width-down/1000?cb=20220819124519');
-  else if (curentMap === 'Olympus')      avatar = ('https://static.wikia.nocookie.net/apexlegends_gamepedia_en/images/3/31/Transition_Olympus_MU1.png/revision/latest/scale-to-width-down/1000?cb=20210504214336');
-  else if (curentMap === "World's Edge") avatar = ('https://static.wikia.nocookie.net/apexlegends_gamepedia_en/images/a/a0/Transition_World%27s_Edge_MU2.png/revision/latest/scale-to-width-down/1000?cb=20200819134346');
-  else if (curentMap === 'Storm Point')  avatar = ('https://static.wikia.nocookie.net/apexlegends_gamepedia_en/images/2/29/Transition_Storm_Point_MU1.png/revision/latest?cb=20220511221733');
-  else if (curentMap === 'Broken Moon')  avatar = ('https://static.wikia.nocookie.net/apexlegends_gamepedia_en/images/6/6d/Broken_Moon_Eternal_Gardens_Concept_Art.jpg/revision/latest?cb=20230130230609');
+  if (curentMap === 'Kings Canyon')
+    avatar =
+      'https://static.wikia.nocookie.net/apexlegends_gamepedia_en/images/2/2d/Transition_Kings_Canyon_MU4.png/revision/latest/scale-to-width-down/1000?cb=20220819124519';
+  else if (curentMap === 'Olympus')
+    avatar =
+      'https://static.wikia.nocookie.net/apexlegends_gamepedia_en/images/3/31/Transition_Olympus_MU1.png/revision/latest/scale-to-width-down/1000?cb=20210504214336';
+  else if (curentMap === "World's Edge")
+    avatar =
+      'https://static.wikia.nocookie.net/apexlegends_gamepedia_en/images/a/a0/Transition_World%27s_Edge_MU2.png/revision/latest/scale-to-width-down/1000?cb=20200819134346';
+  else if (curentMap === 'Storm Point')
+    avatar =
+      'https://static.wikia.nocookie.net/apexlegends_gamepedia_en/images/2/29/Transition_Storm_Point_MU1.png/revision/latest?cb=20220511221733';
+  else if (curentMap === 'Broken Moon')
+    avatar =
+      'https://static.wikia.nocookie.net/apexlegends_gamepedia_en/images/6/6d/Broken_Moon_Eternal_Gardens_Concept_Art.jpg/revision/latest?cb=20230130230609';
 
   let nextMaps = [curentMap, curentRankedMap, nextMap, nextRankedMap].map((e) => {
-    if      (e === 'Kings Canyon') return e = (e + '👑');
-    else if (e === 'Olympus')      return e = (e + '🏛️');
-    else if (e === "World's Edge")  return e = (e + '❄️');
-    else if (e === 'Storm Point')  return e = (e + '🦀');
-    else if (e === 'Broken Moon')  return e = (e + '☄️');
-  })
+    if (e === 'Kings Canyon') return (e = e + '👑');
+    else if (e === 'Olympus') return (e = e + '🏛️');
+    else if (e === "World's Edge") return (e = e + '❄️');
+    else if (e === 'Storm Point') return (e = e + '🦀');
+    else if (e === 'Broken Moon') return (e = e + '☄️');
+  });
 
   curentMap = nextMaps[0];
   curentRankedMap = nextMaps[1];
   nextMap = nextMaps[2];
   nextRankedMap = nextMaps[3];
-  console.log(curentMap, curentRankedMap, nextMap, nextRankedMap);
-  
+
   let infoC = ` till ${times[0].slice(14)} -> ${nextMap}`;
-  let mapTimeC = infoC.slice(6, 10) + infoC.slice(10, 12);
+  let mapTimeC = infoC.slice(6, 10) + infoC.slice(10, 11);
   let infoR = `${curentRankedMap} -> ${nextRankedMap}`;
   let mapTimeR = infoR.slice(7, 9) + infoR.slice(10, 12);
-  
+
   return { infoC, curentMap, mapTimeC, infoR, mapTimeR, avatar };
 };

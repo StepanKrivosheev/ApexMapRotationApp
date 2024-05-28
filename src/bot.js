e819723a90788706ac3497da7043daf2344a497e
George Iyawe <george.iyawe@kernpunkt.de>
2024-05-28 22:00:32 +0200

    feat: add working changes

--- src/bot.js
+++ src/bot.js
@@ -4,8 +4,6 @@ const {
 	Client,
 	GatewayIntentBits,
 	ActivityType,
-	MessageCollector,
-	CategoryChannel,
 } = require('discord.js');
 const getMapInfo = require('./getMapInfo');
 
@@ -13,7 +11,7 @@ dotenv.config();
 botToken = process.env.BOT_TOKEN;
 
 const discordChannels = process.env.DISCORD_CHANNELS;
-const channelsArray = discordChannels.split(',');
+//const channelsArray = discordChannels.split(',');
 
 let mapTimeC = '0';
 let timeOut = '15';
@@ -30,58 +28,64 @@ module.exports = async function bot() {
 	client.login(botToken);
 
 	async function updateNickname() {
-		const apexChannels = {};
 		let mapInfo = await getMapInfo();
 		mapTimeC = mapInfo.mapTimeC;
 		guilds = await client.guilds.fetch();
 		guildIDs = guilds.map((guild) => guild.id);
-		texChannels = {};
-
+		
+		
     
-		let channelsArray = guildIDs.map(async (guildID) => {
-			(await client.guilds.cache.get(guildID).channels.fetch()).toJSON();
+		const getChannel =  async (guildID) => 
+			await client.guilds.cache.get(guildID).channels.fetch()
+		
+	
+
+		const getChannelRequests = guildIDs.map((guildId) => getChannel(guildId));
+		// returns an array of promises. Each promise resolves to an array of channels
+		const allChannels = await Promise.all(getChannelRequests);
+		//returns an array of arrays of channels
+		const allChannelsMerged = allChannels.map((channels) => channels.map((channel) => channel));
+		//returns an array of channels. Removes the nested arrays
+		const allChannelsFlat = allChannelsMerged.flat();
+		const textChannels = allChannelsFlat.filter((channel) => channel.type === 0);
+		const apexChannels = textChannels.filter((channel) => channel.topic && channel.topic.includes('Apex'));
+		
+
+		
+	
+		
+		
+
+		await client.channels.cache
+			.get(textChannels[0].id)
+			.members.get(client.user.id)
+			.setNickname(mapInfo.curentMap);
+		
+		apexChannels.forEach((element) => {
+			console.log(element.id);
+			client.channels.cache
+				.get(element.id)
+				.setTopic(
+					'R.M.: ' +
+						mapInfo.infoR +
+						'\n\n\n\nprovided by: Apex LegendsMapBot\nby KrivS'
+				);
 		});
 
-		console.log(channelsArray);
+		client.user.setPresence({
+			status: 'dnd',
+			activities: [
+				{
+					type: ActivityType.Custom,
+					name: mapInfo.infoC,
+				},
+			],
+		});
 
-		//texChannels.push(channels.find((channel) => channel.type == 0));
-
-		//console.log(texChannels.find((channel) => channel.topic.includes('Apex')));
-
-		// apexChannels.push(
-		// 	texChannels.find((channel) => channel.topic.includes('Apex'))
-		// );
-
-		// await client.channels.cache
-		// 	.get(texChannels[0].id)
-		// 	.members.get(client.user.id)
-		// 	.setNickname(mapInfo.curentMap);
-		// //console.log(apexChannels);
-		// apexChannels.forEach((element) => {
-		// 	console.log(element.id);
-		// 	client.channels.cache
-		// 		.get(element.id)
-		// 		.setTopic(
-		// 			'R.M.: ' +
-		// 				mapInfo.infoR +
-		// 				'\n\n\n\nprovided by: Apex LegendsMapBot\nby KrivS'
-		// 		);
-		// });
-
-		// client.user.setPresence({
-		// 	status: 'dnd',
-		// 	activities: [
-		// 		{
-		// 			type: ActivityType.Custom,
-		// 			name: mapInfo.infoC,
-		// 		},
-		// 	],
-		// });
-
-		// if (timeOut <= 0) {
-		// 	await client.user.setAvatar(mapInfo.avatar);
-		// 	await client.user.setBanner(mapInfo.avatar);
-		// }
+		if (timeOut <= 0) {
+			await client.user.setAvatar(mapInfo.avatar);
+			await client.user.setBanner(mapInfo.avatar);
+		}
 	}
 
 	await client.on('ready', async () => {
